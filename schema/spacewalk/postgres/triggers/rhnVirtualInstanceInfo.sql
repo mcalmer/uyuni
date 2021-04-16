345983a8e23b0fbdead9b0372fe0755baf4ef4da
Michael Calmer <mc@suse.de>
2021-04-16 13:24:28 +0200

    schema: add trigger to change registration required

---
+++ schema/spacewalk/postgres/triggers/rhnVirtualInstanceInfo.sql
@@ -0,0 +1,23 @@
+create or replace function rhn_virtinst_info_iud_trig_fun() returns trigger as
+$$
+begin
+        if tg_op='INSERT' or (tg_op='UPDATE' and old.instance_type is distinct from new.instance_type) then
+                update suseSCCRegCache
+                   set scc_reg_required = 'Y'
+                 where server_id = (select virtual_system_id from rhnvirtualinstance WHERE id = new.instance_id);
+                return new;
+        end if;
+        if tg_op='DELETE' then
+                update suseSCCRegCache
+                   set scc_reg_required = 'Y'
+                 where server_id = (select virtual_system_id from rhnvirtualinstance WHERE id = old.instance_id);
+                return old;
+        end if;
+end;
+$$ language plpgsql;
+
+create trigger
+rhn_virtinst_info_iud_trig
+after insert or update or delete on rhnVirtualInstanceInfo
+for each row
+execute procedure rhn_virtinst_info_iud_trig_fun();
