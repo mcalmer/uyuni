03170bcb72375f7dac261ce83fee01620fb545ce
Michael Calmer <mc@suse.de>
2021-04-26 13:35:07 +0200

    change rhnVirtualInstanceInfo trigger to fire on update only

--- schema/spacewalk/upgrade/susemanager-schema-4.2.9-to-susemanager-schema-4.2.10/156-rhnVirtualInstanceInfo-trigger.sql
+++ schema/spacewalk/upgrade/susemanager-schema-4.2.9-to-susemanager-schema-4.2.10/156-rhnVirtualInstanceInfo-trigger.sql
@@ -1,24 +1,17 @@
 create or replace function rhn_virtinst_info_iud_trig_fun() returns trigger as
 $$
 begin
-        if tg_op='INSERT' or (tg_op='UPDATE' and old.instance_type is distinct from new.instance_type) then
-                update suseSCCRegCache
-                   set scc_reg_required = 'Y'
-                 where server_id = (select virtual_system_id from rhnvirtualinstance WHERE id = new.instance_id);
-                return new;
-        end if;
-        if tg_op='DELETE' then
-                update suseSCCRegCache
-                   set scc_reg_required = 'Y'
-                 where server_id = (select virtual_system_id from rhnvirtualinstance WHERE id = old.instance_id);
-                return old;
-        end if;
+        update suseSCCRegCache
+           set scc_reg_required = 'Y'
+         where server_id = (select virtual_system_id from rhnvirtualinstance WHERE id = new.instance_id);
+        return new;
 end;
 $$ language plpgsql;
 
 drop trigger if exists rhn_virtinst_info_iud_trig on rhnVirtualInstanceInfo;
 create trigger
 rhn_virtinst_info_iud_trig
-after insert or update or delete on rhnVirtualInstanceInfo
+after update on rhnVirtualInstanceInfo
 for each row
+when (OLD.instance_type is distinct from NEW.instance_type)
 execute procedure rhn_virtinst_info_iud_trig_fun();
