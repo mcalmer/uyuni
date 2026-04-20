01e212991a7a949ff8e3d7029dd3a1bbe882fc94
Maxime Noel <maxime.noel@suse.com>
2026-04-20 12:27:38 +1200

    Fix screenshot missing when webdriver is failing during hook

--- testsuite/features/support/env.rb
+++ testsuite/features/support/env.rb
@@ -157,10 +157,22 @@ After do |scenario|
         Capybara.current_session.driver.quit
         visit Capybara.app_host
         log 'Web driver has been restarted'
-      elsif web_session_is_active?
-        handle_screenshot_and_relog(scenario, current_epoch)
       else
-        warn 'There is no active web session; unable to take a screenshot or relog.'
+        # web_session_is_active? can raise WebDriverError if the session went stale
+        # after a long-running step (e.g. bootstrap timeout). Rescue it so the After
+        # hook does not fail and swallow the screenshot opportunity.
+        session_active =
+          begin
+            web_session_is_active?
+          rescue Selenium::WebDriver::Error::WebDriverError => e
+            log "WebDriver session went stale when checking for active session: #{e.message}"
+            false
+          end
+        if session_active
+          handle_screenshot_and_relog(scenario, current_epoch)
+        else
+          warn 'There is no active web session; unable to take a screenshot or relog.'
+        end
       end
     ensure
       print_server_logs
